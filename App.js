c6a45efa701a3296efd46a0bf3f2429aea861367
Aditya Banerjee <aditya_banerjee@icloud.com>
2022-01-03 12:50:03 +0800

    Update landing page

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>An on-demand peer tutoring platform by students, for students.</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f45e61',
     alignItems: 'center',
     justifyContent: 'center',
   },
