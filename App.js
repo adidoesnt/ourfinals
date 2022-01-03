7e6f997badc945ac22c370305fcc8afbeb39e27f
Aditya Banerjee <aditya_banerjee@icloud.com>
2022-01-03 21:17:11 +0800

    Add routing, login and signup pages, and stylesheet

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, 
-  Text, 
+import Login from './src/pages/Login';
+import Signup from './src/pages/Signup';
+
+import {  Text, 
   View, 
   Image, 
   SafeAreaView, 
@@ -8,7 +10,14 @@ import { StyleSheet,
   Alert 
 } from 'react-native';
 
-export default function App() {
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+import { styles } from './src/components/Stylesheet';
+
+const Stack = createStackNavigator();
+
+function Welcome({navigation}) {
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
@@ -19,38 +28,21 @@ export default function App() {
       <Text>An on-demand peer tutoring platform</Text>
       <Text>by students, for students.</Text>
       <View style={styles.join}>
-        <Button
-          title='Log in'
-          onPress={()=>{
-            Alert.alert('Warning', 'Work in progess.',
-            [{text: 'close', onPress: ()=>console.log('Resolved.')}])
-          }}
-        />
-        <Button
-          title='Sign up'
-          onPress={()=>{
-            Alert.alert('Warning', 'Work in progess.',
-            [{text: 'close', onPress: ()=>console.log('Resolved.')}])
-          }}
-        />
+        <Button title='Log in' onPress={()=>{navigation.navigate('Login')}}/>
+        <Button title='Sign up' onPress={()=>{navigation.navigate('Signup')}}/>
       </View>
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f45e61',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  logo: {
-    width: 200,
-    height: 200,
-  },
-  join: {
-    paddingVertical: 10,
-    flexDirection:'row'
-  }
-});
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Welcome'>
+        <Stack.Screen name='Welcome' component={Welcome}/>
+        <Stack.Screen name='Login' component={Login}/>
+        <Stack.Screen name='Signup' component={Signup}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,10 @@
-function Login() {
+import { View, Text, SafeAreaView } from 'react-native';
+import { styles } from '../components/Stylesheet';
 
+export default function Login() {
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text>Log in</Text>
+        </SafeAreaView>
+    );
 }
-
-export default Login;
