d2fd04389516aedaac97576406ae5a9e35474984
ninest <parth.kabra@gmail.com>
2022-03-06 13:26:23 -0500

    Fix module list

--- src/components/ModuleList.js
+++ src/components/ModuleList.js
@@ -1,18 +1,47 @@
-import { ScrollView } from "react-native";
-import ModuleCard from "./cards/ModuleCard"
+import { ScrollView, Text } from "react-native";
+import ModuleCard from "./cards/ModuleCard";
 
 export default function ModuleList(props) {
-    return (
-        <ScrollView>
-            {(Object.entries(props.modules)).forEach(entry => {
-                let moduleId = entry[1].id;
-                let title = entry[1].title;
-                let code = entry[1].moduleCode;
-                let description = entry[1].description;
-                let mcs = entry[1].moduleCredit;
-                <ModuleCard moduleId={moduleId} title={title} code={code} description={description} mcs={mcs}/>
-                console.log('called card generator');
-            })}
-        </ScrollView>
-    );
+  const modules = props.modules.slice(0, 20);
+  return (
+    <ScrollView>
+      {modules &&
+        modules.map((entry) => {
+          let moduleId = entry.id;
+          let title = entry.title;
+          let code = entry.moduleCode;
+          let description = entry.description;
+          let mcs = entry.moduleCredit;
+          return (
+            <ModuleCard
+              key={moduleId}
+              moduleId={moduleId}
+              title={title}
+              code={code}
+              description={description}
+              mcs={mcs}
+            />
+          );
+        })}
+      {/* {Object.entries(modules).forEach((entry) => {
+        let moduleId = entry[1].id;
+        let title = entry[1].title;
+        let code = entry[1].moduleCode;
+        let description = entry[1].description;
+        let mcs = entry[1].moduleCredit;
+
+        console.log("called card generator");
+        return (
+          <Text>{moduleId}</Text>
+          //   <ModuleCard
+          //     moduleId={moduleId}
+          //     title={title}
+          //     code={code}
+          //     description={description}
+          //     mcs={mcs}
+          //   />
+        );
+      })} */}
+    </ScrollView>
+  );
 }
--- src/components/cards/ModuleCard.js
+++ src/components/cards/ModuleCard.js
@@ -1,22 +1,20 @@
-import { View } from 'react-native';
-import { Card } from 'react-native-elements';
+import { View, Text } from "react-native";
+import { Card } from "react-native-elements";
 
 // This component fails to render - the debug statements are not printing @Parth
 export default function ModuleCard(props) {
-    console.log("loading card generator");
-    return (
-        <View>
-            {console.log('generating card')}
-            <Card>
-                <Card.Title>
-                    {props.title}
-                </Card.Title>
-                <Card.Divider/>
-                <Text>{props.moduleId}</Text>
-                <Text>{props.code}</Text>
-                <Text>{props.description}</Text>
-                <Text>{props.mcs}</Text>
-            </Card>
-        </View>
-    );
+  console.log("loading card generator");
+  return (
+    <View>
+      {console.log("generating card")}
+      <Card>
+        <Card.Title>{props.title}</Card.Title>
+        <Card.Divider />
+        <Text>{props.moduleId}</Text>
+        <Text>{props.code}</Text>
+        <Text>{props.description}</Text>
+        <Text>{props.mcs}</Text>
+      </Card>
+    </View>
+  );
 }
