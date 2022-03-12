d2f0d8a797c0b5ef14b922468e5e4ff56a3fc2ac
Aditya Banerjee <aditya_banerjee@icloud.com>
2022-03-12 17:43:35 +0200

    add marketplace functionality

--- src/pages/Marketplace.js
+++ src/pages/Marketplace.js
@@ -1,13 +1,38 @@
-import { SafeAreaView, Text } from "react-native";
-import AssignmentCard from '../components/cards/AssignmentCard';
+import { View, SafeAreaView, ScrollView, Text } from "react-native";
+import { useState, useEffect } from "react";
+import { Card } from 'react-native-elements';
+import AssignmentList from "../components/AssignmentList";
 
-import * as assignments from '../../testData/testAssignments.json';
-const testAssignment = assignments[0];
+const assignmentsData = require('../../testData/testAssignments.json');
 
 export default function Marketplace() {
+    const [assignments, setAssignments] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        let temp = [];
+        for(const key in assignmentsData) {
+            const assignment = {
+                id: key,
+                ...assignmentsData[key]
+            };
+            temp.push(assignment);
+        }
+        setAssignments(temp);
+        setLoading(false);
+    }, []);
+
     return (
-        <SafeAreaView>
-            <AssignmentCard assignmentData={testAssignment}/>
-        </SafeAreaView>
+        <View style={{flex: 1}}>{!loading ? (
+          <ScrollView>
+              <Text>All Assignments</Text>
+              <AssignmentList assignments={assignments}/>
+          </ScrollView>
+      ) : <ScrollView>
+            <Card>
+            <Card.Title>Loading...</Card.Title>
+            </Card>
+          </ScrollView>}
+      </View>
     );
 }
