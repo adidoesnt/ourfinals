c72574a776183c5592a3622de2177798cbfdaf62
Aditya Banerjee <aditya_banerjee@icloud.com>
2022-03-26 15:31:19 +0300

    add comments requesting clarification

--- server/backend/userService.js
+++ server/backend/userService.js
@@ -29,9 +29,17 @@ module.exports = class UserService {
 
   async addStudentAssignment() {
     // this.courseRepository
+    // does this method intend to update the student who submitted the assignment, or add
+    // the assignment to the student's record? if it's the latter, would simply adding 
+    // the assignment to the backend with the linked fields not take care of this?
+    // if it's the former, then I don't think we would have a scenario where we would 
+    // change the student ID associated with the assignment
   }
 
   async addTutorAssignment() {
     // this.courseRepository
+    // does this method intend to update the tutor teaching the assignment, or add
+    // the assignment to the tutor's record? if it's the latter, would simply adding 
+    // the assignment to the backend with the linked fields not take care of this?
   }
 };
